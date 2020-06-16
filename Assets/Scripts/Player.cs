e76ba0456ac6c9dc934c3a2bc374c3d2f530fc9c
Derek Skeba <frillyoz@gmail.com>
2020-06-16 19:18:41 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Start()
+    {
+        var pistolPrefab = Resources.Load<GameObject>("Models/GunPack/FBX/Shotgun_1");
+        var pistolPosition = new Vector3(0.00045f, 0.00315f, -0.0007f);
+        var pistolRotation = new Vector3(-6.352f, 82.234f, -88.08f);
+        var pistolScale = new UnityEngine.Vector3(0.4f, 0.4f, 0.4f);
+        var palmObject = GameObject.FindGameObjectWithTag("PlayerPalm");
+        pistolPrefab.transform.localPosition = pistolPosition;
+        pistolPrefab.transform.localEulerAngles = pistolRotation;
+        pistolPrefab.transform.localScale = pistolScale;
+        Instantiate(pistolPrefab, palmObject.transform);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
